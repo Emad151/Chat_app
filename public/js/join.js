d71d293636d2e71f376333ff467692c88851cb6d
Emad151 <Melad15199@live.missouristate.edu>
2024-01-08 18:44:51 +0200

    url setting

--- public/js/join.js
+++ public/js/join.js
@@ -3,6 +3,7 @@ $roomInput = document.getElementById('room')
 $hint = document.getElementById('hint')
 $submitButton = document.querySelector('button')
 
+const url = 'https://justus-1l4q.onrender.com'
 
 $roomInput.focus()
 $roomInput.addEventListener('input', (e)=>{
@@ -17,7 +18,7 @@ $roomInput.addEventListener('input', (e)=>{
 $usernameInput.addEventListener('input', async(e)=>{
     if ($usernameInput.value.trim() !== "") {
         $hint.textContent = 'Searching for similar name....'
-        const result = await (await fetch(`http://localhost:3000/nameSearch/${$roomInput.value}/${$usernameInput.value}`)).json()
+        const result = await (await fetch(`${url}/nameSearch/${$roomInput.value}/${$usernameInput.value}`)).json()
     
     
     if (result.nameExists) {
